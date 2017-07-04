48cfb8874988cd67a3416dae8d99ecd49d2e04dc
Youri Kopecky <youri@kasual.biz>
2017-07-04 09:01:38 +0200

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :steps
   resources :games
   devise_for :users
+
+  root to: "games#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
