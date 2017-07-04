684cc191a97df9ef625ff8322725853555a73e2f
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-07-04 15:31:15 +0200

    Fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   resources :games do
     post 'start', as: :start
-<<<<<<< HEAD
     resources :steps do
       resources :devices, only: [] do
         get 'validate'
       end
     end
-=======
     post 'reset', as: :reset
-    resources :steps
->>>>>>> 1493ee31c94f84e5407fe9914108b533075a9c1a
   end
   devise_for :users, :controllers => { registrations: 'registrations' }
 
