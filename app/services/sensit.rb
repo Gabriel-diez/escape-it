9132c3638b2e410f069becfc0305b5fef6952a24
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-07-04 12:17:09 +0200

    Improved sensit api manager

--- app/services/sensit.rb
+++ app/services/sensit.rb
@@ -5,7 +5,8 @@ require 'json'
 class Sensit
 
   PATHS = {
-    token: "https://#{Rails.configuration.sensit_api_url}/oauth/token?"
+    token: "https://#{Rails.configuration.sensit_api_url}/oauth/token?",
+    base: "https://#{Rails.configuration.sensit_api_url}/v2/",
   }
 
   def initialize(user)
@@ -13,28 +14,30 @@ class Sensit
   end
 
   def authenticate(code)
-    save post("#{Sensit::PATHS[:token]}grant_type=authorization_code&code=#{code}&client_id=#{@user.client_id}&redirect_uri=http://localhost:3000/callback")
+    save process("#{Sensit::PATHS[:token]}grant_type=authorization_code&code=#{code}&client_id=#{@user.client_id}&redirect_uri=http://localhost:3000/callback")
   end
 
   def refresh
-    save post("#{Sensit::PATHS[:token]}grant_type=refresh_token&client_id=#{@user.client_id}&refresh_token=#{@user.refresh_token}")
+    save process("#{Sensit::PATHS[:token]}grant_type=refresh_token&client_id=#{@user.client_id}&refresh_token=#{@user.refresh_token}", 'post')
+  end
+
+  def devices
+    process("#{Sensit::PATHS[:base]}devices?access_token=#{@user.access_token}")['data']
   end
 
   private
 
-  def post(url)
+  def process(url, method = 'get')
     uri = URI(url)
     Net::HTTP.start(uri.host, uri.port,
       :use_ssl => uri.scheme == 'https') do |http|
-      request = Net::HTTP::Post.new uri
+      request = method == 'get' ? Net::HTTP::Get.new(uri) : Net::HTTP::Post.new(uri)
       request.basic_auth @user.client_id, @user.client_secret
-      response = http.request request
-      JSON.parse(response.body)
+      JSON.parse(http.request(request).body)
     end
   end
 
   def save(tokens)
-    puts tokens
-    @user.update!(access_token: tokens[:access_token], refresh_token: tokens[:refresh_token])
+    @user.update!(access_token: tokens['access_token'], refresh_token: tokens['refresh_token'])
   end
 end
