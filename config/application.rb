193c5160b76ae7e7040ada256255ead5d8ccc250
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-07-04 11:34:31 +0200

    Add controller to manager sensit api

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module EscapeIt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.sensit_api_url = "api.sensit.io"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   end
   devise_for :users, :controllers => { registrations: 'registrations' }
 
+  get '/callback', to: "sensits#callback", as: :callback
+  get '/refresh', to: "sensits#refresh", as: :refresh
+
   root to: "statics#index"
 end
