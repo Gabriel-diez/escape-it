0a3ebb6f8e02c0edfdd92ba70436c89db23b8174
Diez Gabriel <diez.gabriel64@gmail.com>
2017-07-05 14:01:57 +0200

    enable validate

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,5 +1,5 @@
 class DevicesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:validate]
   before_action :set_resources
   before_action :set_device, except: [:validate, :index, :new, :get_sensors]
 
