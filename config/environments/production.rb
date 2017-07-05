63a7b271200af384ba9629f7b6a5187b20769996
Diez Gabriel <diez.gabriel64@gmail.com>
2017-07-05 08:38:17 +0200

    add default url for environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  Rails.application.routes.default_url_options[:host] = 'escape-it.herokuapp.com'
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
